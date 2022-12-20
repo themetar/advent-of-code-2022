ac17b780e01dd08b26f902306cd2afe98c6b2d53
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-12-20 14:59:48 +0100

    Add 04 to tests

--- 04.rb
+++ 04.rb
@@ -1,11 +1,3 @@
-def get_lines(file_path)
-    File.open(file_path, 'r') do |file|
-        file.each_line.collect { |line| line.strip }
-    end
-end
-
-lines = get_lines('inputs/04.txt')
-
 def section_ids(string)
     string.split(/[-,]/).collect { |str| str.to_i }
 end
@@ -18,8 +10,6 @@ def count_contained(lines)
     end
 end
 
-puts count_contained(lines)
-
 def count_overlapped(lines)
     lines.count do |line|
         first_start, first_end, second_start, second_end = section_ids(line)
@@ -27,5 +17,3 @@ def count_overlapped(lines)
         first_start.between?(second_start, second_end) || first_end.between?(second_start, second_end) || second_start.between?(first_start, first_end) || second_end.between?(first_start, first_end)
     end
 end
-
-puts count_overlapped(lines)
--- solutions_test.rb
+++ solutions_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require './common'
 
 # require solution files
-('01'..'03').each do |day|
+('01'..'04').each do |day|
   File.exist?("./#{day}.rb") && require("./#{day}") or break
 end
 
@@ -29,5 +29,13 @@ class SolutionsTest < Minitest::Test
 
     assert sum_priorities(lines) == 7795
     assert sum_badge_priorities(lines) == 2703
+  end
+
+  def test_solution_04_is_correct
+    lines = get_lines('inputs/04.txt')
+
+    assert count_contained(lines) == 483
+
+    assert count_overlapped(lines) == 874
   end
 end
